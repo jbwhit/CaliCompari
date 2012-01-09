0cbc08dcd7a9edcf3e2f54278b9685288668714f
Jonathan Whitmore <jbwhit@gmail.com>
2012-01-09 13:58:56 +1100

    Better continuum fitting implementation

--- Python/FTS-continuum.py
+++ Python/FTS-continuum.py
@@ -12,18 +12,35 @@ import os
 import pylab as pl
 import numpy as np
 from scipy.interpolate import splrep, splev
+import heapq
+
+def smoothContinuum(binsize, nlarge, plot=False):
+  """docstring for smoothContinuum"""
+  iow, iof, inten, kinten = np.loadtxt('sao2010.solref.txt',unpack=True, comments='C')
+  siow = []
+  siof = []
+  for i in xrange(0, len(iof), binsize):
+    siow.append(np.average(iow[i:i + binsize]))
+    siof.append(np.average(heapq.nlargest(nlarge, iof[i:i + binsize])))
+  
+  tck = splrep(siow, siof)
+  np.savetxt('sao2010.contin.ascii', zip(iow, iof/splev(iow,tck)))
+  if plot == True:
+    plot(iow, iof/splev(iow,tck))
+  pass
 
 def main():
-  iow, iof = np.loadtxt('KPNO2010.ascii',unpack=True)
-  binsize = 5000 # FTS pixels
-  miow = []
-  miof = []
-  for i in xrange(0, len(iof), binsize):
-      miow.append(iow[i+np.argmax(iof[i:i + binsize])])
-      miof.append(np.max(iof[i:i + binsize]))
-
-  tck = splrep(miow, miof)
-  np.savetxt('KPNO2010_contin.ascii', zip(iow, iof/splev(iow,tck)))
+  smoothContinuum(200, 1)
+  # iow, iof = np.loadtxt('KPNO2010.ascii',unpack=True)
+  # binsize = 5000 # FTS pixels
+  # miow = []
+  # miof = []
+  # for i in xrange(0, len(iof), binsize):
+  #     miow.append(iow[i+np.argmax(iof[i:i + binsize])])
+  #     miof.append(np.max(iof[i:i + binsize]))
+  # 
+  # tck = splrep(miow, miof)
+  # np.savetxt('KPNO2010_contin.ascii', zip(iow, iof/splev(iow,tck)))
   pass
 
 
