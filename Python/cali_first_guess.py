75412bd0bd5397161de176487be7e0185e624986
Jonathan Whitmore <jonathan.b.whitmore@gmail.com>
2016-07-03 23:47:04 -0700

    Explicit type casting from command line

--- Python/cali_first_guess.py
+++ Python/cali_first_guess.py
@@ -2,7 +2,7 @@
 
 # Usage: cali_first_guess [shift] [slope] [sigma] [multiple] [offset]
 # Units are:              [Ang.]  [???]   [km/s?] [None]     [Norm.]
-# Reasonable defaults:    0.001   -0.002  3.0     1.37       0.002
+# Reasonable values:      0.001   -0.002  3.0     1.37       0.002
 
 import sys
 import json
@@ -11,23 +11,23 @@ import json
 #print 'Argument List:', str(sys.argv)
 
 first_guesses = {}
-first_guesses.update({'shift':sys.argv[1],
+first_guesses.update({'shift':float(sys.argv[1]),
                       'fix_shift':False,
                       'limit_shift':(-1.5, 1.5),
                       'error_shift':0.03})
-first_guesses.update({'slope':sys.argv[2],
+first_guesses.update({'slope':float(sys.argv[2]),
                       'fix_slope':False,
                       'limit_slope':(-2.0, 2.0),
                       'error_slope':0.04})
-first_guesses.update({'sigma':sys.argv[3],
+first_guesses.update({'sigma':float(sys.argv[3]),
                       'fix_sigma':False,
                       'limit_sigma':(1.0, 10.0),
                       'error_sigma':0.2})
-first_guesses.update({'multiple':sys.argv[4],
+first_guesses.update({'multiple':float(sys.argv[4]),
                       'fix_multiple':False,
                       'limit_multiple':(0.1, 20.0),
                       'error_multiple':0.03})
-first_guesses.update({'offset':sys.argv[5],
+first_guesses.update({'offset':float(sys.argv[5]),
                       'fix_offset':False,
                       'limit_offset':(-2.0, 2.0),
                       'error_offset':0.03})
@@ -35,4 +35,3 @@ first_guesses.update({'minuit':0, 'fix_minuit':True})
 
 with open("first_guesses.json", 'w') as file_handle:
     json.dump(first_guesses, file_handle, indent=2, sort_keys=True)
-
